d636f580d82c80d66b56a12c4679e35d80491ebb
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2025-12-15 15:11:18 +0000

    Edit version file names

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Get the Vocabulary Plus version to upgrade to
-VP_CURRENT=$(cat $INSTALL_DIR/versions/vp/cur.txt)
+VP_CURRENT=$(cat $INSTALL_DIR/versions/vp/current.txt)
 VP_LATEST=$(cat $INSTALL_DIR/versions/vp/latest.txt)
 # Set the Vocabulary Plus upgrade flag
 if [ "$VP_CURRENT" != "$VP_LATEST" ]; then
@@ -12,7 +12,7 @@ else
 fi
 
 # Get the Vocabulary Plus Version Manager version to upgrade to
-VM_CURRENT=$(cat $INSTALL_DIR/versions/vp-vm/cur.txt)
+VM_CURRENT=$(cat $INSTALL_DIR/versions/vp-vm/current.txt)
 VM_LATEST=$(cat $INSTALL_DIR/versions/vp-vm/latest.txt)
 # Set the Vocabulary Plus Version Manager upgrade flag
 if [ "$VM_CURRENT" != "$VM_LATEST" ]; then
