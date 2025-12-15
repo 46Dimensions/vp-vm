63a68b5ab047bea85db94fe13e2f173723358746
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2025-12-15 15:22:37 +0000

    Add colours and header

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
+# ANSI colours
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+reset="\033[0m"
+
+echo "${green}===============================================${reset}"
+echo "${green}Vocabulary Plus Version Manager: Upgrader 1.0.0${reset}"
+echo "${green}===============================================${reset}"
+
 # Get the Vocabulary Plus version to upgrade to
 VP_CURRENT=$(cat $INSTALL_DIR/versions/vp/current.txt)
 VP_LATEST=$(cat $INSTALL_DIR/versions/vp/latest.txt)
