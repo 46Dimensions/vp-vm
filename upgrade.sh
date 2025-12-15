08d9eca4c75bcf01512725b079c333880b91175c
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2025-12-15 15:51:58 +0000

    Add upgrade and upgrade summary

--- upgrade.sh
+++ upgrade.sh
@@ -30,3 +30,77 @@ if [ "$VM_CURRENT" != "$VM_LATEST" ]; then
 else
     UPGRADE_VM=false
 fi
+
+# Upgrade current Vocabulary Plus if needed
+if [ "$UPGRADE_VP" = true ]; then
+    echo "${yellow}Uninstalling current Vocabulary Plus version...${reset}"
+    # Move the JSON files and VM into a temporary location
+    cd $INSTALL_DIR
+    cd .. # Move into VocabularyPlus parent directory
+    cd .. # Move into VocabularyPlus's parent directory
+    mkdir VocabularyPlusTemp
+    mv VocabularyPlus/JSON VocabularyPlusTemp/JSON
+    mv VocabularyPlus/vm VocabularyPlusTemp/vm
+    # Run the Vocabulary Plus uninstaller
+    sh VocabularyPlus/uninstall
+    echo "${green}Current Vocabulary Plus version uninstalled.${reset}"
+    echo ""
+    # Install the latest version
+    echo "${yellow}Installing latest Vocabulary Plus version...${reset}"
+    curl -fsSL https://raw.githubusercontent.com/46Dimensions/VocabularyPlus/main/install.sh -o install.sh
+    sh install.sh
+    rm install.sh
+    # Move the JSON files and VM back into VocabularyPlus directory
+    if [ -d "VocabularyPlus/JSON" ]; then
+        rm -rf VocabularyPlus/JSON
+    fi
+
+    if [ -d "VocabularyPlus/vm" ]; then
+        rm -rf VocabularyPlus/vm
+    fi
+    mv VocabularyPlusTemp/JSON VocabularyPlus/JSON
+    mv VocabularyPlusTemp/vm VocabularyPlus/vm
+    rm -rf VocabularyPlusTemp
+    # Set the current version to the latest version
+    echo $VP_LATEST > $INSTALL_DIR/versions/vp/current.txt
+    echo "${green}Latest Vocabulary Plus version installed.${reset}"
+    echo ""
+fi
+
+# Upgrade Vocabulary Plus Version Manager if needed
+if [ "$UPGRADE_VM" = true ]; then
+    echo "${yellow}Upgrading Vocabulary Plus Version Manager...${reset}"
+    # Move the Vocabulary Plus version files into a temporary location
+    cd $INSTALL_DIR
+    cd .. # Move into VocabularyPlus parent directory
+    mkdir vm-temp
+    mv $INSTALL_DIR/versions/vp vm-temp/vp
+    # Run the VP VM uninstaller
+    sh vm/uninstall.sh
+    echo "${green}Current Vocabulary Plus Version Manager uninstalled.${reset}"
+    echo ""
+    # Install the latest VP VM version
+    echo "${yellow}Installing latest Vocabulary Plus Version Manager...${reset}"
+    curl -fsSL https://raw.githubusercontent.com/46Dimensions/vp-vm/main/install-vm.sh -o install.sh
+    sh install.sh
+    rm install.sh
+    # Move the Vocabulary Plus version files back into the versions directory
+    mv vm-temp/vp $INSTALL_DIR/versions/vp
+    rm -rf vm-temp
+    # Set the current version to the latest version
+    echo $VM_LATEST > $INSTALL_DIR/versions/vp-vm/current.txt
+    echo "${green}Latest Vocabulary Plus Version Manager installed.${reset}"
+    echo ""
+fi
+
+echo "${green}Upgrade Summary${reset}"
+echo "${green}---------------${reset}"
+if [ "$UPGRADE_VP" = true ]; then
+    echo "Vocabulary Plus ${red}$VP_CURRENT${reset} -> ${green}$VP_LATEST${reset}"
+fi
+if [ "$UPGRADE_VM" = true ]; then
+    echo "Vocabulary Plus Version Manager ${red}$VM_CURRENT${reset} -> ${green}$VM_LATEST${reset}"
+fi
+if [ "$UPGRADE_VP" = false ] && [ "$UPGRADE_VM" = false ]; then
+    echo "0 packages upgraded."
+fi
