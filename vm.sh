421c96837166ff8fdb6669fdcfbc1f0360e948f2
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2025-12-17 08:48:07 +0000

    Add vm.sh

---
+++ vm.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+# ANSI colours
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+reset="\033[0m"
+
+# Header
+echo "${green}=======================================${reset}"
+echo "${green}Vocabulary Plus Version Manager (0.1.0)${reset}"
+echo "${green}=======================================${reset}"
+
+# Check command ($1)
+case "$1" in
+    update)
+        sh "$INSTALL_DIR/upgrade.sh"
+        ;;
+    upgrade)
+        sh "$INSTALL_DIR/upgrade.sh"
+        ;;
+    --version)
+        echo "0.1.0"
+        ;;
+    --help)
+        echo "Usage: vp-vm [command]"
+        echo ""
+        echo "Commands:"
+        echo "   update      Update the version lists"
+        echo "   upgrade     Upgrade Vocabulary Plus and vp-vm to the latest version"
+        echo "   --version   Show the installed version of Vocabulary Plus Version Manager"
+        echo "   --help      Show this help message and exit"
+        exit 0
+        ;;
+    *)
+        echo "${red}Command "$1" not recognised.${reset}"
+        echo "Usage: vp-vm [command]"
+        echo ""
+        echo "Commands:"
+        echo "   update      Update the version lists"
+        echo "   upgrade     Upgrade Vocabulary Plus and vp-vm to the latest version"
+        echo "   --version   Show the installed version of Vocabulary Plus Version Manager"
+        echo "   --help      Show a help message and exit"
+        exit 1
+        ;;
+esac
