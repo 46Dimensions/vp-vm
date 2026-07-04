1607680235cefcc0fb21d9048d09b0c22449eac4
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-07-04 14:13:57 +0100

    Fix escape sequence formatting in Write-Logo function

--- install-vm.ps1
+++ install-vm.ps1
@@ -12,11 +12,11 @@ if (-not $InstallDir) {
 
 function Write-Logo {
     $esc = [char]27
-    Write-Host "${esc}[38;5;99m🭖█🭀  🭋█🭡   ${esc}[38;5;171m██████🭎"
-    Write-Host "${esc}[38;5;105m🭦█🭐  🭅█🭛   ${esc}[38;5;177m██   🭨█🭬"
-    Write-Host "${esc}[38;5;141m 🭖█🭀🭋█🭡   ${esc}[38;5;183m ██████🭡"
-    Write-Host "${esc}[38;5;177m 🭦█🭐🭅█🭛   ${esc}[38;5;209m ██"
-    Write-Host "${esc}[38;5;209m  🭖██🭡   ${esc}[38;5;220m  ██${esc}[0m"
+    Write-Host "$esc[38;5;99m🭖█🭀  🭋█🭡   $esc[38;5;171m██████🭎"
+    Write-Host "$esc[38;5;105m🭦█🭐  🭅█🭛   $esc[38;5;177m██   🭨█🭬"
+    Write-Host "$esc[38;5;141m 🭖█🭀🭋█🭡   $esc[38;5;183m ██████🭡"
+    Write-Host "$esc[38;5;177m 🭦█🭐🭅█🭛   $esc[38;5;209m ██"
+    Write-Host "$esc[38;5;209m  🭖██🭡   $esc[38;5;220m  ██$esc[0m"
     Write-Host "VOCABULARY PLUS"
     Write-Host "Version Manager: Windows Installer (1.1.0)"
     Write-Host ""
