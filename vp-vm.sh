978874869be5e527fb6d640440ba1d421d78f68f
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-08-04 12:30:15 +0100

    Add debugging to show_info function

--- vp-vm.sh
+++ vp-vm.sh
@@ -457,7 +457,14 @@ show_info() {
 
     if [ "$version" = "" ]; then
         current_version=$(cat "$MAIN_DIR/current.txt")
-        latest_version=$(list_remote_versions | get_latest_version)
+
+        remote_versions=$(list_remote_versions)
+        echo "DEBUG: remote_versions = '$remote_versions'"
+        latest_version=$(printf '%s' "$remote_versions" | get_latest_version)
+        echo "DEBUG: latest_version (method 1) = '$latest_version'"
+        latest_version_method_2=$(list_remote_versions | get_latest_version)
+        echo "DEBUG latest_Version (method 2) = '$latest_version_method_2'"
+
         count=$(list_installed_versions | grep -c '.')
 
         echo "VP VM v$VP_VM_VERSION"
