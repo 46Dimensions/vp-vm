406ccb6ffb9f01cea1bce214f9dbf82d2a610830
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-09-03 15:41:40 +0100

    Add Out-Null to directory creation in vp-vm.ps1

--- vp-vm.ps1
+++ vp-vm.ps1
@@ -29,9 +29,9 @@ $VERSIONS_DIR = Join-Path "$MAIN_DIR" "versions"
 $VP_VM_VERSION = Get-Content (Join-Path $MAIN_DIR "version.txt")
 $WINDOWS_VERSION = (Get-ComputerInfo -Property WindowsDisplayVersion).WindowsDisplayVersion
 
-New-Item -ItemType Directory -Path $MAIN_DIR -Force
-New-Item -ItemType Directory -Path $DOWNLOAD_DIR -Force
-New-Item -ItemType Directory -Path $VERSIONS_DIR -Force
+New-Item -ItemType Directory -Path $MAIN_DIR -Force | Out-Null
+New-Item -ItemType Directory -Path $DOWNLOAD_DIR -Force | Out-Null
+New-Item -ItemType Directory -Path $VERSIONS_DIR -Force | Out-Null
 
 function Write-Logo {
     $esc = [char]27
