429ab92e4e807f7323c5e3a5a35deb70667dc651
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-09-04 12:22:09 +0100

    Fix Windows version detection in vp-vm.ps1

--- vp-vm.ps1
+++ vp-vm.ps1
@@ -30,7 +30,9 @@ $VERSIONS_DIR = Join-Path "$MAIN_DIR" "versions"
 
 $VP_VM_VERSION = Get-Content (Join-Path $MAIN_DIR "version.txt")
 $VP_VM_DISPLAY_VERSION = Get-Content (Join-Path $MAIN_DIR "version-display.txt")
-$WINDOWS_VERSION = (Get-ComputerInfo -Property WindowsDisplayVersion).WindowsDisplayVersion
+
+$computerInfo = Get-ComputerInfo -Property WindowsProductName, WindowsVersion, OsBuildNumber
+$WINDOWS_VERSION = "$($computerInfo.WindowsProductName) $($computerInfo.WindowsVersion) (build $($computerInfo.OsBuildNumber))"
 
 New-Item -ItemType Directory -Path $MAIN_DIR -Force | Out-Null
 New-Item -ItemType Directory -Path $DOWNLOAD_DIR -Force | Out-Null
