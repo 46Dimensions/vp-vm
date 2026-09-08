fe98b3c28247864f2e34bbcce881b7acab12a805
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2026-09-08 12:14:31 +0100

    Remove `vp-vm` executable when uninstalling on Unix

--- vp-vm.sh
+++ vp-vm.sh
@@ -484,6 +484,7 @@ uninstall_self() {
         rm -rf "$HOME/.vp-vm"
         rm -f "$BIN_DIR/vocabularyplus"
         rm -f "$BIN_DIR/vp"
+        rm -f "$BIN_DR/vp-vm"
     else
         write_info "Uninstallation cancelled."
     fi
